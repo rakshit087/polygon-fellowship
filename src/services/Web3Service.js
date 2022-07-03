d4f85caecf2dd3c9ed44dc9ab49d118dac2d3aad
Rakshit <rakshit087@gmail.com>
2022-07-04 00:30:22 +0530

    Added Web3 Services

--- src/services/Web3Service.js
+++ src/services/Web3Service.js
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-
+import vaultArtifact from "../artifacts/src/contracts/Vault.sol/Vault.json";
 const vaultAddress = "0x0000000000000000000000000000000000000000";
 
 export const Web3Service = {
@@ -25,5 +25,42 @@ export const Web3Service = {
     const signer = provider.getSigner();
     const wallet = await signer.getAddress();
     return wallet;
+  },
+
+  //Get current collateral balance
+  getCollateralBalance: async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const vault = new ethers.Contract(vaultAddress, vaultArtifact.abi, signer);
+    const balance = await vault.getCollateral();
+    return balance;
+  },
+
+  //get current Debt
+  getDebt: async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const vault = new ethers.Contract(vaultAddress, vaultArtifact.abi, signer);
+    const debt = await vault.getDebt();
+    return debt;
+  },
+
+  //Borrow DINR
+  borrowDINR: async (amount) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const vault = new ethers.Contract(vaultAddress, vaultArtifact.abi, signer);
+    const tx = await vault.borrowDINR({
+      value: ethers.utils.parseUnits(amount.toString(), "ether"),
+    });
+    return tx;
+  },
+  //Return DINR
+  returnDINR: async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const vault = new ethers.Contract(vaultAddress, vaultArtifact.abi, signer);
+    const tx = await vault.returnDINR();
+    return tx;
   },
 };
