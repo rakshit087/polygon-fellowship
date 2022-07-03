b45c7d3314fbd265e6087068cc6c202a6cc7e929
Rakshit <rakshit087@gmail.com>
2022-07-04 03:07:30 +0530

    Fixed DINR Token

--- src/contracts/DINRToken.sol
+++ src/contracts/DINRToken.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DINRToken is ERC20, ERC20Burnable, Ownable {
+contract DINRToken is ERC20, Ownable {
     constructor() ERC20("DINRToken", "DINR") {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+    }
+
+    function burn(address to, uint256 amount) external onlyOwner {
+        _burn(to, amount);
     }
 }
