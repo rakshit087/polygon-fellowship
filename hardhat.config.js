8c4f6ffcc5a4b4f971b07fd8c5fd03960ab69de4
Rakshit <rakshit087@gmail.com>
2022-07-03 19:36:00 +0530

    Updated Solidity

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.7",
   defaultNetwork: "ropsten",
   networks: {
     hardhat: {},
