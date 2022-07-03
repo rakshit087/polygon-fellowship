a49b2378d7e6f582f4fb1bb804855dd3772e31dc
Rakshit <rakshit087@gmail.com>
2022-07-04 02:56:02 +0530

    Completed Borrow Form

--- src/components/BorrowForm.js
+++ src/components/BorrowForm.js
@@ -1,12 +1,25 @@
 import { Box, Button, Typography, TextField } from "@mui/material";
+import { useState } from "react";
+import { Web3Service } from "../services/Web3Service";
 
 export const BorrowForm = () => {
+  const [amount, setAmount] = useState("");
   return (
-    <Box sx={{display:"flex", flexDirection:"column"}}>
-      <TextField label="Enter Collatoral in ETH" />
-      <Button variant="contained" color="primary" onClick={()=>{
-        console.log("borrow")
-      }}> Borrow </Button>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <TextField
+        label="Enter Collatoral in ETH"
+        onChange={(e) => setAmount(e.target.value)}
+        type="number"
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          Web3Service.borrowDINR(amount);
+        }}
+      >
+        Borrow
+      </Button>
     </Box>
   );
 };
