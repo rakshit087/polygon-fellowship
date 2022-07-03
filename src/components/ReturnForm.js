330f6e52c22cf27609f0d3caf76e34345a7ae84e
Rakshit <rakshit087@gmail.com>
2022-07-04 03:06:52 +0530

    Completed Return Form

--- src/components/ReturnForm.js
+++ src/components/ReturnForm.js
@@ -1,12 +1,30 @@
 import { Box, Button, Typography, TextField } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Web3Service } from "../services/Web3Service";
 
 export const ReturnForm = () => {
+  const [amount, setAmount] = useState(0);
+  useEffect(() => {
+    Web3Service.getDebt().then((debt) => {
+      setAmount(parseInt(debt) / 10 ** 18);
+    });
+  }, []);
+
   return (
-    <Box sx={{display:"flex", flexDirection:"column"}}>
-      <TextField label="O DINR Due" disabled />
-      <Button variant="contained" color="primary" onClick={()=>{
-        console.log("borrow")
-      }}> Return </Button>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <TextField label={`${amount.toString()} DINR DUE`} disabled />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          Web3Service.returnDINR().then((tx) => {
+            console.log(tx);
+          });
+        }}
+      >
+        {" "}
+        Return{" "}
+      </Button>
     </Box>
   );
 };
